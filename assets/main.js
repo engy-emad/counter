e7a7d9e222064f23f2dab895a57bb3273bcceaff
engy emad <103267344+Engy123456@users.noreply.github.com>
2022-07-03 11:54:46 +0200

    add style and js

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+let count = 0;
+let counter = document.getElementById("counter");
+console.log(counter);
+
+function minus() {
+    if (count == 0) {
+        count = 0;
+
+    } else {
+        count = count - 1;
+    }
+
+    counter.innerHTML = count;
+
+}
+
+
+function plus() {
+    count = count + 1;
+    counter.innerHTML = count;
+}
